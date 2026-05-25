465d062c584a9280c3de40f162f474dc420e0ad2
Karan Solanki <solankikaran05@gmail.com>
2026-05-25 20:38:04 +0530

    Bronze layer finished

--- dags/flight-pipeline.py
+++ dags/flight-pipeline.py
@@ -1,17 +1,30 @@
+import os
 import sys
+
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow.sdk import dag, task
 
+AIRFLOW_HOME = Path("/opt/airflow")
+
+if str(AIRFLOW_HOME) not in sys.path:
+    sys.path.insert(0, str(AIRFLOW_HOME))
+
 from scripts.bronze_ingest import run_bronze_ingestion
 
-AIRFLOW_HOME = Path("/opt/airflow")
+default_args = {
+    "owner": "airflow",
+    "retries": 0,
+    "retry_delay" : timedelta(minutes=5)
+}
 
 @dag(
     dag_id="flight_ops_medallion_pipeline",
     start_date=datetime(2026, 5, 25),
-    schedule = "@daily"
+    default_args=default_args,
+    schedule = "@daily",
+    catchup=False
 )
 def flight_ops_medallion_pipeline():
 
--- scripts/bronze_ingest.py
+++ scripts/bronze_ingest.py
@@ -3,9 +3,11 @@ import json
 from datetime import datetime
 from pathlib import Path
 
+from airflow.sdk import get_current_context
+
 URL = "https://opensky-network.org/api/states/all"
 
-def run_bronze_ingestion(context: Context):
+def run_bronze_ingestion():
     response = requests.get(URL, timeout=30)
     response.raise_for_status()
     data = response.json()
@@ -16,9 +18,5 @@ def run_bronze_ingestion(context: Context):
 
     with open(path, "w") as f:
         json.dump(data, f)
-    
-    # XCOM
-    context["ti"].xcom_push(
-        key="bronze_file_path",
-        value = str(path)
-    )
+
+    return str(path)
